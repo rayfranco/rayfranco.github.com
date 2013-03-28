d5314dac98718045880e7026d472ccb061a9f901
Victor Coulon <victor@hull.io>
2013-03-28 12:15:49 +0100

    remove useless github client id

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 require(['aura/aura'], function (Aura) {
   var app = new Aura({
-    github: { client_id: '60c7ab102b827976136d' }
+    github: { }
   });
   app.use('assets/js/extensions/aura-github');
   app.use('assets/js/extensions/aura-time');
